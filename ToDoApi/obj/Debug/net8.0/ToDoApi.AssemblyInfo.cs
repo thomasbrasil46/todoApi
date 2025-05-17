87d00df155efaec4ba99fd3fc283ec18013dcfe6
thomasbrasil46 <thomasbrasilpb@gmail.com>
2025-05-17 02:21:12 -0300

    Adiciona controlador e suporte a scaffolding e EF Core
    Foi adicionado o controlador `TodoItemsController` para gerenciar
    itens de tarefas em uma API RESTful, implementando operações CRUD
    usando o Entity Framework Core. Foram incluídas dependências como
    `Microsoft.EntityFrameworkCore.Tools` e `Microsoft.VisualStudio.Web.CodeGeneration.Design`
    para suporte a scaffolding e migrações.
    
    Além disso, foram adicionados arquivos de configuração (`appsettings.json`
    e `appsettings.Development.json`) para logs, exemplos de requisições
    HTTP no arquivo `ToDoApi.http`, e documentação OpenAPI/Swagger para
    os endpoints da API.
    
    Outras mudanças incluem a adição de bibliotecas para internacionalização,
    análise de código e geração de código, além de atualizações em arquivos
    de configuração e cache para refletir as alterações no projeto.

--- ToDoApi/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
+++ ToDoApi/obj/Debug/net8.0/ToDoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab34d743d82326b60643dccc6ad5c2c76d8df0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
